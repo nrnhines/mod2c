1bb7e254324d6c399bb77c155cabfd80032c463e
Aleksandr Ovcharenko <aleksandr.ovcharenko@epfl.ch>
2015-07-01 12:13:01 +0200

    Updating CMake related files, adding CPackConfig
    Change-Id: Iaa0d35cc5202f7a8b0a7a27915d699022008979d

---
+++ CMake/CPackConfig.cmake
@@ -0,0 +1,11 @@
+# @author  Stefan Eilemann
+# @remarks Copyright (c) BBP/EPFL 2011; All rights reserved. Do not distribute without further notice.
+
+# General CPack configuration 
+# Info: http://www.itk.org/Wiki/CMake:Component_Install_With_CPack
+# _____________________________________________________________________________
+
+set(CPACK_PACKAGE_CONTACT "Sam Yates <Sam.Yates@epfl.ch")
+set(CPACK_PACKAGE_LICENSE "Proprietary")
+ 
+include(CommonCPack)
