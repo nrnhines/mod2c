eba3440192797a02945e845ddb2c9f5728ae3f4c
Michael Hines <michael.hines@yale.edu>
2016-08-18 10:34:01 -0400

    Openacc framework for KINETIC and derivimplicit that avoids function pointer arguments. Still need to declare global instead of static.
    Change-Id: I558fb9ed03e260e7855372c6f36f38ef7b5c7b92

--- src/mod2c_core/deriv.c
+++ src/mod2c_core/deriv.c
@@ -160,15 +160,31 @@ dindepname, fun->name, listnum, listnum);
 	replacstr(qsol, buf);
 #if VECTORIZE
 	if (method->subtype & DERF) { /* derivimplicit */
-Sprintf(buf,"%s %s%s_thread(%d, _slist%d, _dlist%d, %s, _threadargs_);\n%s",
-deriv1_advance, ssprefix, method->name,
+Sprintf(buf,
+"%s"
+"  #if !defined(_derivimplic_%s)\n"
+"    #define _derivimplic_%s 0\n"
+"  #endif\n"
+"  #pragma acc routine(%s%s_thread) seq\n"
+"  %s%s_thread(%d, _slist%d, _dlist%d, _derivimplic_%s, _threadargs_);\n"
+"%s",
+deriv1_advance,
+fun->name, fun->name,
+ssprefix, method->name, ssprefix, method->name,
 numeqn, listnum, listnum, fun->name,
 deriv2_advance);
 	vectorize_substitute(qsol, buf);
 	}else{ /* kinetic */
    if (vectorize) {
-Sprintf(buf, "%s%s_thread(&_thread[_spth%d]._pvoid, %d, _slist%d, _dlist%d, &%s, %s, %s\
-, _linmat%d, _threadargs_);\n",
+Sprintf(buf,
+"\n"
+"  #if !defined(_kinetic_%s)\n"
+"    #define _kinetic_%s 0\n"
+"  #endif\n"
+"  #pragma acc routine(%s%s_thread) seq\n"
+"  %s%s_thread(&_thread[_spth%d]._pvoid, %d, _slist%d, _dlist%d, &%s, %s, _kinetic_%s, _linmat%d, _threadargs_);\n",
+fun->name, fun->name,
+ssprefix, method->name,
 ssprefix, method->name, listnum, numeqn, listnum, listnum, indepsym->name,
 dindepname, fun->name, listnum);
 	vectorize_substitute(qsol, buf);
